95e8cdadb78405f896e04b8f5cf83ca8ae9bb718
bola-cloud <bola.ishak41@gmail.com>
2025-06-30 04:20:27 +0300

    orders

--- database/migrations/2025_06_26_010412_create_orders_table.php
+++ database/migrations/2025_06_26_010412_create_orders_table.php
@@ -14,15 +14,15 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->enum('type', ['follow', 'like']);
-            $table->unsignedInteger('total_count');  // total likes or follows
-            $table->unsignedInteger('done_count')->default(0); // completed likes or follows
-            $table->integer('cost')->default(0); // remaining likes or follows
+            $table->unsignedInteger('total_count');
+            $table->unsignedInteger('done_count')->default(0);
+            $table->integer('cost')->default(0);
             $table->enum('status', ['active', 'paused', 'completed'])->default('active');
-            $table->text('target_url')->nullable(); // URL to target for likes or follows
-            $table->unsignedBigInteger('user_id'); // user who created the order
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('target_url'); // Instagram profile or post link
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Creator of the order
             $table->timestamps();
-                // Index for filtering
+
+            // Index for filtering
             $table->index('target_url');
         });
     }
