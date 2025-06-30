e7fba5dd62c5e1bd83329efa62a545c338aef66e
bola-cloud <bola.ishak41@gmail.com>
2025-06-30 04:19:37 +0300

    orders

--- database/migrations/2025_06_26_010412_create_orders_table.php
+++ database/migrations/2025_06_26_010412_create_orders_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->unsignedInteger('done_count')->default(0); // completed likes or follows
             $table->integer('cost')->default(0); // remaining likes or follows
             $table->enum('status', ['active', 'paused', 'completed'])->default('active');
-            $table->text('targetUrl')->nullable(); // URL to target for likes or follows
+            $table->text('target_url')->nullable(); // URL to target for likes or follows
             $table->unsignedBigInteger('user_id'); // user who created the order
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
