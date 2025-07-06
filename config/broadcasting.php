d02076af4e51eff68fe1699d8685871647666efc
bola-cloud <bola.ishak41@gmail.com>
2025-07-06 16:22:26 +0300

    broad

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,11 +36,11 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => false,
                 'host' => env('PUSHER_HOST', '127.0.0.1'),
                 'port' => env('PUSHER_PORT', 6001),
                 'scheme' => env('PUSHER_SCHEME', 'http'),
-                'encrypted' => false,
-                'useTLS' => false,
             ],
             'client_options' => [
                 'timeout' => 10,
