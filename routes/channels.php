a8a4d3c0bf5a8063613b9a01b471548199f2cd0a
bola-cloud <bola.ishak41@gmail.com>
2025-07-07 05:09:09 +0300

    broad

--- routes/channels.php
+++ routes/channels.php
@@ -7,8 +7,13 @@ Broadcast::channel('orders.{user_id}', function ($user, $userId) {
 });
 
 Broadcast::channel('actions.{user_id}', function ($user, $userId) {
+    \Log::info('Broadcast Auth', [
+        'auth_user_id' => $user?->id,
+        'channel_user_id' => $userId,
+    ]);
     return $user && (int) $user->id === (int) $userId;
 });
+
 
 Broadcast::channel('presence.active.users', function ($user) {
     return $user ? ['id' => $user->id, 'name' => $user->name] : false;
