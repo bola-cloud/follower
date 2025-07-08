97e5e4d66357511662a2f749e825f72a729fbc40
bola-cloud <bola.ishak41@gmail.com>
2025-07-08 20:25:02 +0300

    broad

--- resources/views/test-active.blade.php
+++ resources/views/test-active.blade.php
@@ -1,15 +1,23 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Dashboard - Active Users</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body class="bg-gray-100">
 
-@section('content')
-<div class="container">
-    <h1>WebSocket Active Users</h1>
+    <div class="container">
+        <h1>WebSocket Active Users</h1>
 
-    <div class="card mt-4">
-        <div class="card-body">
-            <h4>Currently connected users:
-                <span class="badge bg-primary">{{ $activeConnections }}</span>
-            </h4>
+        <div class="card mt-4">
+            <div class="card-body">
+                <h4>Currently connected users:
+                    <span class="badge bg-primary">{{ $activeConnections }}</span>
+                </h4>
+            </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
