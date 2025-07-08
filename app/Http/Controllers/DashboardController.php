b05de84797f9b9c2fe23cf093ee278f7818dc4f3
bola-cloud <bola.ishak41@gmail.com>
2025-07-08 20:13:55 +0300

    broad

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function activeUsers()
+    {
+        $response = Http::withHeaders([
+            'X-CSRF-TOKEN' => csrf_token(),
+        ])->post(url('/laravel-websockets/statistics'));
+
+        if ($response->successful()) {
+            $data = $response->json();
+            $activeConnections = $data['peak_connection_count'] ?? 0;
+
+            return view('dashboard.active-users', compact('activeConnections'));
+        }
+
+        return view('dashboard.active-users', ['activeConnections' => 0]);
+    }
+}
