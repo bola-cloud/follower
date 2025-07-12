20f388bfa264bacb902c864f6706a84a660ed391
bola-cloud <bola.ishak41@gmail.com>
2025-07-12 15:30:22 +0300

    websocket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,13 +22,12 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    wssPort: import.meta.env.VITE_PUSHER_PORT,
-    forceTLS: import.meta.env.VITE_PUSHER_SCHEME === 'https',
+    key: 'localkey123',
+    wsHost: 'egfollow.com',
+    wsPort: 443,
+    wssPort: 443,
+    forceTLS: true,
     encrypted: true,
     disableStats: true,
-    wsPath: import.meta.env.VITE_PUSHER_APP_PATH,
+    wsPath: '/app',
 });
