4bb4cc92065a4ded8cc7ee4db84369b873b425e1
bola-cloud <bola.ishak41@gmail.com>
2025-07-13 16:07:57 +0300

    link

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -87,7 +87,7 @@ class OrderService
             ]);
 
             $escapedPayload = escapeshellarg($payload);
-            \Log::error('❌ Failed to create pending actions:', ['payload' => $escapedPayload]);
+            \Log::error('payload data sent:', ['payload' => $escapedPayload]);
             // ✅ topic = orders/{order_id}/{user_id}
             exec("node node_scripts/mqtt_order_publisher.cjs {$escapedPayload} > /dev/null 2>&1 &");
         }
