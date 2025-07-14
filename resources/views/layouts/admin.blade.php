999832282b9678b7b70cbe5413caaeb511837466
bola-cloud <bola.ishak41@gmail.com>
2025-07-15 02:30:28 +0300

    subscribe

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -67,10 +67,13 @@ data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
         <ul class="nav navbar-nav flex-row">
           <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
           <li class="nav-item mr-auto">
-            <a class="navbar-brand" href="{{route('admin.dashboard')}}">
-              <img class="brand-logo"  src="{{asset('app-assets/images/instagram.jpeg')}}">
-              <h3 class="brand-text">EG Followers </h3>
+            <a class="navbar-brand d-flex align-items-center" href="{{ route('admin.dashboard') }}">
+                {{-- <img class="brand-logo mr-1" src="{{ asset('app-assets/images/instagram.jpeg') }}" alt="Logo" style="height: 30px;"> --}}
+                <h3 class="brand-text mb-0 d-flex align-items-center">
+                    <i class="fab fa-instagram me-1"></i> EG Followers
+                </h3>
             </a>
+
           </li>
           <li class="nav-item d-none d-md-block float-right"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="toggle-icon ft-toggle-right font-medium-3 white" data-ticon="ft-toggle-right"></i></a></li>
           <li class="nav-item d-md-none">
