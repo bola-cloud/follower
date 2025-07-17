8b1f42527d83f263cfa16b0a0519a55f8af248bb
bola-cloud <bola.ishak41@gmail.com>
2025-07-17 20:30:43 +0300

    dashboard

--- app/Http/Controllers/Admin/Dashboard.php
+++ app/Http/Controllers/Admin/Dashboard.php
@@ -16,19 +16,19 @@ class Dashboard extends Controller
 {
     public function index(Request $request)
     {
-        // // ✅ 1. Clear previous cache (set expires in controller, optional)
-        // Cache::put('device_activations_set', [], now()->addMinutes(2));
-        // Cache::put('device_activations_count', 0, now()->addMinutes(2));
+        // ✅ 1. Clear previous cache (set expires in controller, optional)
+        Cache::put('device_activations_set', [], now()->addMinutes(2));
+        Cache::put('device_activations_count', 0, now()->addMinutes(2));
 
-        // // ✅ 2. Run the Node.js script to trigger MQTT
-        // $process = new Process(['node', base_path('node_scripts/mqtt_ping_devices.cjs')]);
+        // ✅ 2. Run the Node.js script to trigger MQTT
+        $process = new Process(['node', base_path('node_scripts/mqtt_ping_devices.cjs')]);
 
-        // try {
-        //     $process->mustRun();
-        //     logger('✅ MQTT ping script executed successfully');
-        // } catch (ProcessFailedException $e) {
-        //     logger()->error('❌ MQTT ping failed: ' . $e->getMessage());
-        // }
+        try {
+            $process->mustRun();
+            logger('✅ MQTT ping script executed successfully');
+        } catch (ProcessFailedException $e) {
+            logger()->error('❌ MQTT ping failed: ' . $e->getMessage());
+        }
 
 
         // Statistics
