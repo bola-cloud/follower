48a48b4e91995e9f7db216f693f79fb044127d07
bola-cloud <bola.ishak41@gmail.com>
2025-08-04 02:02:14 +0300

    new transaction limit

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,8 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('/trigger-test-order', [SoketiTestController::class, 'triggerTestOrder']);
 Route::post('/trigger-test-response', [SoketiTestController::class, 'triggerTestResponse']);
 
+Route::get('/orders/{order_id}/eligible-users', [OrderController::class, 'eligibleUsers']);
+
 // Protected Routes (Require authentication)
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
@@ -41,7 +43,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/user/profile-link', [AuthController::class, 'updateProfileLink']);
     Route::post('/promocode/redeem', [PromocodeController::class, 'redeem']);
     Route::get('/user/points', [AuthController::class, 'points']);
-    Route::get('/orders/{order_id}/eligible-users', [OrderController::class, 'eligibleUsers']);
 });
 Route::post('/mqtt/response', [\App\Http\Controllers\Api\MqttResponseController::class, 'handle']);
 Route::post('/mqtt/recalculate-orders', [\App\Http\Controllers\Api\MqttResponseController::class, 'recalculateAllOrders']);
