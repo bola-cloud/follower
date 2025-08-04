eb0a42f6c1d1a1edfc96c9aa5966777dacc29c71
bola-cloud <bola.ishak41@gmail.com>
2025-08-04 03:54:10 +0300

    new transaction limit

--- routes/api.php
+++ routes/api.php
@@ -43,7 +43,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/user/profile-link', [AuthController::class, 'updateProfileLink']);
     Route::post('/promocode/redeem', [PromocodeController::class, 'redeem']);
     Route::get('/user/points', [AuthController::class, 'points']);
-    Route::get('/users/process-active-orders', [OrderController::class, 'processActiveUserOrders']);
+    Route::post('/user/process-active-orders', [OrderController::class, 'processActiveUserOrders']);
 });
 Route::post('/mqtt/response', [\App\Http\Controllers\Api\MqttResponseController::class, 'handle']);
 Route::post('/mqtt/recalculate-orders', [\App\Http\Controllers\Api\MqttResponseController::class, 'recalculateAllOrders']);
