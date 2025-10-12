d60e0e4a937de667a594ddf3ac0fcab0cdd6d8ba
bola-cloud <bola.ishak41@gmail.com>
2025-10-12 16:55:00 +0300

    fix redis issue

--- mqtt_load_test.py
+++ mqtt_load_test.py
@@ -126,7 +126,7 @@ def main(start_id, end_id):
 
 # Example: Simulate devices with uIds from 5000 to 5100
 if __name__ == "__main__":
-    start_id = 15134  # Your start ID
-    end_id = 15634  # Your end ID
+    start_id = 15648  # Your start ID
+    end_id = 25647  # Your end ID
     main(start_id, end_id)
 #end_id = 15634
