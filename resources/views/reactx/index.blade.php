b884a36b873949fd413d53be4a642079367dfb04
bola-cloud <bola.ishak41@gmail.com>
2025-12-20 04:44:10 +0200

    apk dashboard

--- resources/views/reactx/index.blade.php
+++ resources/views/reactx/index.blade.php
@@ -233,7 +233,7 @@
     });
 
     function loadApkData() {
-      fetch('{{ route("adminapk.stats") }}')
+      fetch('{{ route("admin.apk.stats") }}')
         .then(response => response.json())
         .then(data => {
           document.getElementById('total-downloads').textContent = data.total_downloads.toLocaleString();
